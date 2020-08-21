82eec102c05c4bc198ab14eb75ce1a43dfe106be
Nils Geistmann <nils@geistmann.eu>
2020-08-21 12:08:26 +0200

    uses vuetify data table

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <div />
+<v-app>
+  <v-data-table
+    :headers="headers"
+    :items="consumables"
+    class="elevation-1"
+  ></v-data-table>
+</v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
-    //
+    headers: [
+      {
+        text: "Type",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      {
+        text: "Count",
+        align: "start",
+        sortable: false,
+        value: "count",
+      },
+    ],
+    consumables: [
+      {
+        name: "Beer",
+        count: 20,
+      },
+      {
+        name: "Coke",
+        count: 2,
+      },
+    ],
   }),
 };
 </script>
