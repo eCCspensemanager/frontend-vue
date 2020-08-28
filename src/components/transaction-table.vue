96afe02b9a15a3bd5e15b6e7bfbdd720213169d7
Dennis <dennis.breitling@codecentric.de>
2020-08-28 11:05:46 +0200

    adds create functionality

--- src/components/transaction-table.vue
+++ src/components/transaction-table.vue
@@ -1,11 +1,45 @@
 <template>
-  <v-data-table :headers="headers" :items="transactions"></v-data-table>
+  <v-data-table :headers="headers" :items="transactions">
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">New Item</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.count" label="Count"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
   name: "TransactionTable",
   data: () => ({
+    dialog: false,
     headers: [
       {
         text: "Type",
@@ -20,10 +54,49 @@ export default {
         value: "count",
       },
     ],
+    data: false,
+    editedItem: {
+      name: "",
+      count: 0,
+    },
+    defaultItem: {
+      name: "",
+      count: 0,
+    },
   }),
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+  },
+
   computed: {
     transactions: function () {
       return this.$store.state.transactions;
+    },
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
+
+  methods: {
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        // TODO edit
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.$store.commit("addTransaction", this.editedItem);
+      }
+      this.close();
     },
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ export default new Vuex.Store({
     ]
   },
   mutations: {
-    add(state, transaction) { state.transactions.push(transaction) }
+    addTransaction(state, transaction) { state.transactions.push(transaction) }
   },
   actions: {
   },
