b4b038f68a3f9faca1dbbff7d25c0549f5f7fd20
Dennis Breitling <breitling@posteo.de>
2020-11-06 09:21:32 +0100

    #28: removes CategoryState from generic mock helper

--- src/tests/mock.ts
+++ src/tests/mock.ts
@@ -3,7 +3,7 @@ import { CategoryState } from '@/components/category/store';
 export function mockGetters(originalGetters: any) {
   let mockObject: any = Object.assign({}, originalGetters);
   Object.keys(mockObject).forEach((key) => {
-    mockObject[key] = (_: CategoryState) => jest.fn();
+    mockObject[key] = (_: any) => jest.fn();
   });
   return mockObject;
 }
